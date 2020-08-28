cbc832e4d1a41254275428e60a3a07f9a7b705ae
Raphael THIRIET <thiriet.raphael@gmail.com>
2020-08-29 00:55:31 +0200

    update deploy to manage Docker data

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,15 @@ mv ../action-view/public/assets/app*.js ../action-view/public/scripts/app.js
 rm ../action-view/public/assets/*.map
 #mv ../public/assets/app*.js.map ../public/scripts/app.js.map
 
+rm ../action-view/docker/web/action-view/public/assets/*
+
+cp ./dist/* ../action-view/docker/web/action-view/public/assets/
+
+rm ../action-view/docker/web/action-view/public/scripts/app*
+
+mv ../action-view/public/assets/common.js ../action-view/docker/web/action-view/public/scripts/common.js
+mv ../action-view/public/assets/app*.js ../action-view/docker/web/action-view/public/scripts/app.js
+
+rm ../action-view/docker/web/action-view/public/assets/*.map
+
 echo 'deploy complete!'
